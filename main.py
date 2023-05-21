c50bbd5efe074643e8f0b665df568fede5712d3d
atonaJS <atona.yk.develop@gmail.com>
2023-05-22 00:12:14 +0900

    ActivityPubの仕様見直し

--- main.py
+++ main.py
@@ -5,30 +5,25 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-
-@app.get("/test")
-def test():
-    content = {"message": "Hello World"}
-    return JSONResponse(content=content)
+server = "activity-pub-server.onrender.com"
+actor = "test"
 
 
-@app.get("/")
+@app.get(f"/users/{actor}")
 def person():
     content = {
-        "@context": "https://www.w3.org/ns/activitystreams",
+        "@context": [
+            "https://www.w3.org/ns/activitystreams",
+            "https://w3id.org/security/v1",
+        ],
+        "id": f"https://{server}/users/{actor}",  # Fediverseで一意
         "type": "Person",
-        "id": "https://activity-pub-server.onrender.com/test",  # Fediverseで一意
-        "name": "test",  # 表示名
-        "preferredUsername": "test",  # ユーザID
+        "url": f"https://{server}/users/{actor}",  # プロフィールページのURL
         "summary": "my simple activitypub",  # 概要
-        "inbox": "https://activity-pub-server.onrender.com/inbox",  # このユーザへの宛先
-        "outbox": "https://activity-pub-server.onrender.com/outbox",  # このユーザの発信元
-        "url": "https://activity-pub-server.onrender.com/test",  # プロフィールページのURL
-        "icon": {
-            "type": "Image",
-            "mediaType": "image/png",  # mime type
-            "url": "https://activity-pub-server.onrender.com/icon.ong",  # アイコン画像のURL
-        },
+        "preferredUsername": f"{actor}",  # ユーザID
+        "name": "actor river",  # 表示名
+        "inbox": f"https://{server}/users/{actor}/inbox",  # このユーザへの宛先
+        "outbox": f"https://{server}/users/{actor}/outbox",  # このユーザの発信元
     }
     headers = {"Content-Type": "application/activity+json"}
     return JSONResponse(content=content, headers=headers)
@@ -36,24 +31,37 @@ def person():
 
 @app.get("/.well-known/host-meta")
 def webfinger_host_meta():
-    data = """<?xml version="1.0"?>
-        <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
-            <Link rel="lrdd" type="application/xrd+xml" template="https://activity-pub-server.onrender.com/.well-known/webfinger?resource={uri}"/>\
-        </XRD>
-        """
-    # dom = parseString(xml_str)
+    data = (
+        '<?xml version="1.0"?>\
+        <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">\
+            <Link rel="lrdd" type="application/xrd+xml" template="'
+        + "https://"
+        + server
+        + '/.well-known/webfinger?resource={uri}"/>\
+        </XRD>'
+    )
     return Response(content=data, media_type="application/xml")
 
 
 @app.get("/.well-known/webfinger")
 def webfinger_resource():
     content = {
-        "subject": "acct:test@activity-pub-server.onrender.com",
+        "subject": f"acct:{actor}@{server}",
+        "aliases": [f"https://{server}/@{actor}", f"https://{server}/users/{actor}"],
         "links": [
+            {
+                "rel": "http://webfinger.net/rel/profile-page",
+                "type": "text/html",
+                "href": f"https://{server}/@{actor}",
+            },
             {
                 "rel": "self",
                 "type": "application/activity+json",
-                "href": "https://activity-pub-server.onrender.com/test",
+                "href": f"https://{server}/users/{actor}",
+            },
+            {
+                "rel": "http://ostatus.org/schema/1.0/subscribe",
+                "template": f"https://{server}/authorize_interaction?uri={{uri}}",
             },
         ],
     }
